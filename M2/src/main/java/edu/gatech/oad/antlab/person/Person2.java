4e2e73494c4ed350021b7d1ac9a212617e0b1697
Shreya Keshive <shreyakeshive@gatech.edu>
2017-09-01 23:47:41 -0400

    Added required method to Person2.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -3,7 +3,7 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -29,16 +29,22 @@ public class Person2 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
-	}
+   private static String calc(String input) {
+     char[] charInput = input.toCharArray();
+     for (int i = 0; i < charInput.length; i++) {
+       int randomIndex = (int)(Math.random() * charInput.length);
+       char oldChar = charInput[i];
+       charInput[i] = charInput[randomIndex];
+       charInput[randomIndex] = oldChar;
+     }
+     return new String(charInput);
+   }
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
