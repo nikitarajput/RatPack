4c432e363cfbc01a9aeaabe88fae222a06068a9c
AaronMcDaniel <aaronmcdaniel7@gmail.com>
2017-09-06 11:54:47 -0400

    Added my name and GTID to antlabmain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,14 +67,14 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Jacob Kim");
 		  toPrint += p3.toString("jkim3213");
-      //Person4 replace P4 with your name
-      //and gburdell4 with your gt id
-      Person4 p4 = new Person4("P4");
-      toPrint += p4.toString("gburdell4");
-          //Person5 replace P5 with your name
-          //and gburdell5 with your gt id
-          Person5 p5 = new Person5("Nikita Rajput");
-          toPrint += p5.toString("nrajput7");
+		  //Person4 replace P4 with your name
+		  //and gburdell4 with your gt id
+		  Person4 p4 = new Person4("Aaron McDaniel");
+		  toPrint += p4.toString("amcdaniel39");
+		  //Person5 replace P5 with your name
+		  //and gburdell5 with your gt id
+		  Person5 p5 = new Person5("Nikita Rajput");
+		  toPrint += p5.toString("nrajput7");
 
         System.out.println(toPrint);
 
