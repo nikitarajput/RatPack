daa773ec608847845b8887c2a82598bcc8c5675f
AaronMcDaniel <AaronMcDaniel7@gmail.com>
2017-09-20 15:11:36 -0400

    this is the first commit of the branch. I have a dummy app with the login, register, and home views

---
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
@@ -0,0 +1,13 @@
+package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class Register_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_);
+    }
+}
