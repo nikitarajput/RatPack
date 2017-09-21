0432335c05e57ae3378ad4a318d9e919fb6defa8
AaronMcDaniel <AaronMcDaniel7@gmail.com>
2017-09-20 20:01:52 -0400

    updated the .xml's so they display right

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Home_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Home_Activity.java
@@ -8,6 +8,6 @@ public class Home_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_);
+        //setContentView(R.layout.activity_home_);
     }
 }
--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
@@ -8,6 +8,6 @@ public class Register_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_);
+        //setContentView(R.layout.activity_register_);
     }
 }
