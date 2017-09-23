7eef99da528ca8116534a7244170fb905905d8c9
nikitarajput <nrajput@gatech.edu>
2017-09-23 15:52:38 -0400

    added login authentication

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Login_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Login_Activity.java
@@ -4,19 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Login_Activity extends AppCompatActivity {
+
+    EditText username, password;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+        username = (EditText)findViewById(R.id.username_editText);
+        password = (EditText)findViewById(R.id.password_editText);
     }
 
     public void login(View v) {
-        //will need to verify username/password
-        startActivity(new Intent(Login_Activity.this, Home_Activity.class));
+        if(username.getText().toString().equals("user") && password.getText().toString().equals("test")) {
+            Toast.makeText(getApplicationContext(), "Logging in...",Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Login_Activity.this, Home_Activity.class));
+        } else {
+            Toast.makeText(getApplicationContext(), "Incorrect username or password.",Toast.LENGTH_SHORT).show();
+            username.setText("");
+            password.setText("");
+            username.requestFocus();
+        }
     }
 
     public void toRegisterActivity(View v){
