5d1d6188118aca755f39c77680d3cf060f88b5b7
Soni Aggarwal <soniaggarwal@lawn-128-61-117-65.lawn.gatech.edu>
2017-09-29 11:56:56 -0400

    Started adding functionality for registration

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
@@ -4,18 +4,35 @@ package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
+        import android.widget.EditText;
+
+        import java.util.HashMap;
+        import java.util.Map;
 
 
 public class Register_Activity extends AppCompatActivity {
+
+    EditText username, password, confirmPassword;
+    Map<String, String> accountInformation = new HashMap<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
+        username = (EditText)findViewById(R.id.register_username_editText);
+        password = (EditText)findViewById(R.id.register_password_editText);
+        confirmPassword = (EditText)findViewById(R.id.confirm_password_editText);
     }
 
     public void register(View v){
-        //will need to add account info to library of usernames/passwords
+        if(!password.getText().toString().equals(confirmPassword.getText().toString())) {
+            // need to say that passwords don't match and try again
+        } else  if (accountInformation.containsKey(username.getText().toString())) {
+            // need to say that account exists for that username
+        } else {
+            accountInformation.put(username.getText().toString(), password.getText().toString());
+            // need to say success and do the redirect
+        }
         startActivity(new Intent(Register_Activity.this, Home_Activity.class));
     }
 
