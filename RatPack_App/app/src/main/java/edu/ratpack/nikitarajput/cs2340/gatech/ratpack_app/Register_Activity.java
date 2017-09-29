eea9b5c4d9f0c062ff413714fbbc615d46ca4a9c
Soni Aggarwal <soniaggarwal@lawn-128-61-117-65.lawn.gatech.edu>
2017-09-29 14:39:35 -0400

    Registration refactoring

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
@@ -1,22 +1,16 @@
 package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.EditText;
-        import android.widget.Toast;
-
-        import java.util.HashMap;
-        import java.util.Map;
-        import java.util.Set;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Register_Activity extends AppCompatActivity {
 
     EditText username, password, confirmPassword;
-    Map<String, String> accountInformation = new HashMap<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,23 +21,51 @@ public class Register_Activity extends AppCompatActivity {
         confirmPassword = (EditText)findViewById(R.id.confirm_password_editText);
     }
 
+    /**
+     * Registers the user.
+     *
+     * @param v the current view that the data is coming from.
+     */
     public void register(View v){
-        if(!password.getText().toString().equals(confirmPassword.getText().toString())) {
-            Toast.makeText(getApplicationContext(), "Passwords do not match.",Toast.LENGTH_SHORT).show();
-            password.setText("");
-            confirmPassword.setText("");
-            password.requestFocus();
-        } else if (accountInformation.containsKey(username.getText().toString())) {
-            Toast.makeText(getApplicationContext(), "Account already exists for this username.",Toast.LENGTH_SHORT).show();
+        if (verifyConfirmPassword() && verifyUniqueUsername()) {
+            User addedUser = new User(username.getText().toString(), password.getText().toString(), false, username.getText().toString());
+            // need to add this user to the database
+            Toast.makeText(getApplicationContext(), "Registering your account...",Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Register_Activity.this, Login_Activity.class));
+        }
+    }
+
+    /**
+     * Verifies that the username is not already in the database.
+     *
+     * @return a boolean whether or not the username is unique.
+     */
+    private boolean verifyUniqueUsername() {
+        if (false) { // need to check if username is already in database
+            Toast.makeText(getApplicationContext(), "Account already exists for this username.", Toast.LENGTH_SHORT).show();
             username.setText("");
             password.setText("");
             confirmPassword.setText("");
             username.requestFocus();
-        } else {
-            accountInformation.put(username.getText().toString(), password.getText().toString());
-            Toast.makeText(getApplicationContext(), "Registering your account...",Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(Register_Activity.this, Login_Activity.class));
+            return false;
         }
+        return true;
+    }
+
+    /**
+     * Verifies that the password and confirm password are the same.
+     *
+     * @return a boolean whether or not the password is the same.
+     */
+    private boolean verifyConfirmPassword() {
+        if (!password.getText().toString().equals(confirmPassword.getText().toString())) {
+            Toast.makeText(getApplicationContext(), "Passwords do not match.",Toast.LENGTH_SHORT).show();
+            password.setText("");
+            confirmPassword.setText("");
+            password.requestFocus();
+            return false;
+        }
+        return true;
     }
 
     public void toWelcomeActivity(View v){
