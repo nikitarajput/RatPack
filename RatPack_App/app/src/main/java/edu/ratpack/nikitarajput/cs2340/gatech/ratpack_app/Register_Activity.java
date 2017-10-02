5a12063b0d001e3933f283b5fd0f525687646f79
nikitarajput <nrajput@gatech.edu>
2017-10-01 21:36:09 -0400

    firebase added on registration

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
@@ -2,10 +2,18 @@ package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +22,9 @@ import java.util.Map;
 public class Register_Activity extends AppCompatActivity {
 
     EditText username, password, confirmPassword, email;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "EmailPassword";
     Map<String, User> currentUsers = new HashMap<>();
 
     @Override
@@ -24,6 +35,19 @@ public class Register_Activity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.register_password_editText);
         confirmPassword = (EditText)findViewById(R.id.confirm_password_editText);
         email = (EditText)findViewById(R.id.email_address_editText);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
     }
 
     /**
@@ -45,16 +69,29 @@ public class Register_Activity extends AppCompatActivity {
             username.requestFocus();
         }
         else if(!verifyConfirmPassword());
-        else if(!verifyConfirmPassword());
+        else if(!verifyUniqueUsername());
         else if(email.getText().toString().length() < 3){
             Toast.makeText(getApplicationContext(), "Email must contain at least 3 characters", Toast.LENGTH_SHORT).show();
             email.requestFocus();
         }
         else {
-            User addedUser = new User(username.getText().toString(), password.getText().toString(), email.getText().toString());
-            currentUsers.put(addedUser.getUsername(), addedUser);
-            Toast.makeText(getApplicationContext(), "Registering your account...",Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(Register_Activity.this, Login_Activity.class));
+            mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(Register_Activity.this, "Registration successful!.",
+                                            Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(Register_Activity.this, Login_Activity.class));
+                                } else {
+                                    Toast.makeText(Register_Activity.this, "Authentication failed. Please try again.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
         }
     }
 
