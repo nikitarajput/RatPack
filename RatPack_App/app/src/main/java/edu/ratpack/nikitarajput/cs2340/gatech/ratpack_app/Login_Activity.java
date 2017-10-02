be31c87a0703513812ce1232487a2dc3dfd92f2a
nikitarajput <nrajput@gatech.edu>
2017-10-01 22:56:07 -0400

    further firebase authentication

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Login_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Login_Activity.java
@@ -1,16 +1,26 @@
 package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Login_Activity extends AppCompatActivity {
 
     EditText username, password;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPasswordLogIn";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,17 +28,24 @@ public class Login_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_login_);
         username = (EditText)findViewById(R.id.username_editText);
         password = (EditText)findViewById(R.id.password_editText);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void login(View v) {
-        if(username.getText().toString().equals("user") && password.getText().toString().equals("pass")) {
-            Toast.makeText(getApplicationContext(), "Logging in...",Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(Login_Activity.this, Home_Activity.class));
-        } else {
-            Toast.makeText(getApplicationContext(), "Incorrect username or password.",Toast.LENGTH_SHORT).show();
-            password.setText("");
-            password.requestFocus();
-        }
+        mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login_Activity.this, Home_Activity.class));
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(Login_Activity.this, "Incorrect username or password. Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void toWelcomeActivity(View v){
--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Register_Activity.java
@@ -21,11 +21,8 @@ import java.util.Map;
 
 public class Register_Activity extends AppCompatActivity {
 
-<<<<<<< HEAD
-    EditText username, password, confirmPassword, email;
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private static final String TAG = "EmailPassword";
+    private static final String TAG = "EmailPasswordRegistration";
 
     EditText username, password, confirmPassword;
     Map<String, User> currentUsers = new HashMap<>();
@@ -37,20 +34,7 @@ public class Register_Activity extends AppCompatActivity {
         username = (EditText)findViewById(R.id.register_username_editText);
         password = (EditText)findViewById(R.id.register_password_editText);
         confirmPassword = (EditText)findViewById(R.id.confirm_password_editText);
-        email = (EditText)findViewById(R.id.email_address_editText);
         mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-            }
-        };
     }
 
     /**
@@ -59,23 +43,13 @@ public class Register_Activity extends AppCompatActivity {
      * @param v the current view that the data is coming from.
      */
     public void register(View v) {
-        if (username.getText().toString().length() > 16 || username.getText().toString().length() < 4) {
-            Toast.makeText(getApplicationContext(), "Username must be of length 4-16", Toast.LENGTH_SHORT).show();
-            username.requestFocus();
-        } else if (password.getText().toString().length() > 16 || password.getText().toString().length() < 6) {
-            Toast.makeText(getApplicationContext(), "Password must be of length 6-16", Toast.LENGTH_SHORT).show();
-            password.requestFocus();
-        } else if (!verifyConfirmPassword()) ;
-        else if (email.getText().toString().length() < 3) {
-            Toast.makeText(getApplicationContext(), "Email must contain at least 3 characters", Toast.LENGTH_SHORT).show();
-            email.requestFocus();
-        } else {
-            mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+        if (verifyUsernameLength() || verifyPasswordLength() || verifyConfirmPassword()) {
+            mAuth.createUserWithEmailAndPassword(username.getText().toString(), password.getText().toString())
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                                Log.d(TAG, "createUserWithEmail" + task.isSuccessful());
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 Toast.makeText(Register_Activity.this, "Registration successful!.",
                                         Toast.LENGTH_SHORT).show();
@@ -86,25 +60,9 @@ public class Register_Activity extends AppCompatActivity {
                             }
                         }
                     });
+        } else {
+            Toast.makeText(getApplicationContext(), "Registration unsuccessful. Please try again.", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    /**
-     * Verifies that the username is not already in the database.
-     *
-     * @return boolean true if the username is unique and false otherwise.
-     *
-     */
-    private boolean verifyUniqueUsername() {
-        if (currentUsers.containsKey(username.getText().toString())) {
-            Toast.makeText(getApplicationContext(), "Account already exists for this username.", Toast.LENGTH_SHORT).show();
-            username.setText("");
-            password.setText("");
-            confirmPassword.setText("");
-            username.requestFocus();
-            return false;
-        }
-        return true;
     }
 
     /**
