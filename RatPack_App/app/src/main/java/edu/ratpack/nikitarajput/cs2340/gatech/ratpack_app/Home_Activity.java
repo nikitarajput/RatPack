32b9df079514684c15f3bd38546f86c3addd5942
nikitarajput <nrajput@gatech.edu>
2017-10-01 23:06:04 -0400

    added firebase logout functionality

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Home_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Home_Activity.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Home_Activity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onLogout(View v) {
+        mAuth.signOut();
         startActivity(new Intent(Home_Activity.this,Welcome_Activity.class));
     }
 }
