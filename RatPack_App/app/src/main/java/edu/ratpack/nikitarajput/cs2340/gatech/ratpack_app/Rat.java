c356025938f0ff40d82a87d1d82055949d131e23
Soni Aggarwal <soniaggarwal@lawn-128-61-117-65.lawn.gatech.edu>
2017-10-06 14:02:51 -0400

    Finished Rat class except for getting unique key from Firebase.

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Rat.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Rat.java
@@ -1,6 +1,10 @@
 package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
 
+import android.location.Location;
+
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by soniaggarwal on 10/6/17.
@@ -23,8 +27,8 @@ public class Rat {
         // set the unique key from firebase
         this.longitude = longitude;
         this.latitude = latitude;
-        // set date
-        // set time
+        this.date = createDate();
+        this.time = createTime();
         this.locationType = locationType;
         this.zipCode = zipCode;
         this.address = address;
@@ -32,7 +36,207 @@ public class Rat {
         this.borough = borough;
     }
 
-    // do getters and setters here
+    /**
+     * Creates the current date.
+     *
+     * @return the current date.
+     */
+    private String createDate() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("MMM dd, yyyy");
+        return timeFormat.format(calendar.getTime());
+    }
+
+    /**
+     * Creates the current time.
+     *
+     * @return the current time.
+     */
+    private String createTime() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(calendar.getTime());
+    }
+
+    /**
+     * Gets the unique key.
+     *
+     * @return the unique key.
+     */
+    public String getUniqueKey() {
+        return this.uniqueKey;
+    }
+
+    /**
+     * Sets the unique key.
+     *
+     * @param uniqueKey the unique key to be set.
+     */
+    public void setUniqueKey(String uniqueKey) {
+        this.uniqueKey = uniqueKey;
+    }
+
+    /**
+     * Gets the latitude.
+     *
+     * @return the latitude.
+     */
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    /**
+     * Sets the latitude.
+     *
+     * @param latitude the the latitude to be set.
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * Gets the longitude.
+     *
+     * @return the longitude.
+     */
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    /**
+     * Sets the longitude.
+     *
+     * @param longitude the the longitude to be set.
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * Gets the date.
+     *
+     * @return the date.
+     */
+    public String getDate() {
+        return this.date;
+    }
+
+    /**
+     * Sets the date.
+     *
+     * @param date the date to be set.
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * Gets the time.
+     *
+     * @return the time.
+     */
+    public String getTime() {
+        return this.time;
+    }
+
+    /**
+     * Sets the time.
+     *
+     * @param time the time to be set.
+     */
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    /**
+     * Gets the location type.
+     *
+     * @return the location type.
+     */
+    public LocationType getLocationType() {
+        return this.locationType;
+    }
+
+    /**
+     * Sets the location type.
+     *
+     * @param locationType the location type to be set.
+     */
+    public void setLocationType(LocationType locationType) {
+        this.locationType = locationType;
+    }
+
+    /**
+     * Gets the zip code.
+     *
+     * @return the zip code.
+     */
+    public int getZipCode() {
+        return this.zipCode;
+    }
+
+    /**
+     * Sets the zip code.
+     *
+     * @param zipCode the zip code to be set.
+     */
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    /**
+     * Gets the address.
+     *
+     * @return the address.
+     */
+    public String getAddress() {
+        return this.address;
+    }
+
+    /**
+     * Sets the address.
+     *
+     * @param address the addresss to be set.
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Gets the city.
+     *
+     * @return the city.
+     */
+    public String getCity() {
+        return this.city;
+    }
+
+    /**
+     * Sets the city.
+     *
+     * @param city the city to be set.
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * Gets the borough.
+     *
+     * @return the borough.
+     */
+    public Borough getBorough() {
+        return this.borough;
+    }
+
+    /**
+     * Sets the borough.
+     *
+     * @param borough the borough to be set.
+     */
+    public void setBorough(Borough borough) {
+        this.borough = borough;
+    }
 
     public enum LocationType implements Serializable {
         FAMILY_DWELLING("1-2 Family Dwelling"),
