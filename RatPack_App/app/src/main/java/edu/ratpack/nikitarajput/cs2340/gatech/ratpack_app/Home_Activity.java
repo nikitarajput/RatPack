c0ca158dba71c5873a45bff9a9d471cf1382aea3
AaronMcDaniel <AaronMcDaniel7@gmail.com>
2017-10-05 22:23:14 -0400

    I need someone to test that this works on their laptop before I merge to master. I updated the SDK for Android Studio, it gave me errors but I fixed them. I added the view for the list view of rat sightings. It will display a variable number of buttons depending on the number of rat sightings. I also gave it a reload button if we want that. We can limit it to 50 most recent. I also made the home page display your email so we know it is the right user.

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Home_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Home_Activity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,10 +18,18 @@ public class Home_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_);
         mAuth = FirebaseAuth.getInstance();
+
+        //shows that you are the right user
+        TextView temp = (TextView)findViewById(R.id.login_status_textView);
+        temp.setText(temp.getText()+"\n"+mAuth.getCurrentUser().getEmail());
     }
 
     public void onLogout(View v) {
         mAuth.signOut();
         startActivity(new Intent(Home_Activity.this,Welcome_Activity.class));
+    }
+    public void toRatSightings(View v){
+        startActivity(new Intent(Home_Activity.this, Rat_Sightings_Activity.class));
+
     }
 }
---
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Rat_Sightings_Activity.java
@@ -0,0 +1,108 @@
+package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+//import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.*;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class Rat_Sightings_Activity extends AppCompatActivity {
+    //this will be replaced with our list of rats from firebase
+    //all methods involving the list of rats will be fully implemented in the Rat class
+    //currently they are just place holders for testing/debugging
+    static String[] oldRatListStandIn = new String[]{"Sleepy", "Ratty", "Fluffy", "Dopey"};
+    static String[] ratListStandIn = new String[]{"Newbie", "Sleepy", "Ratty", "Fluffy", "Dopey"};
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rat_sightings);
+
+        Rat_Sightings_Activity.setRatList();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_rat_sightings);
+        for(int i = 0; i<ratListStandIn.length; i++){//adds the buttons to the Layout
+            Button butt = new Button(this);
+
+            butt.setText("Rat: "+ratListStandIn[i]);//changed later
+            butt.setId(i);
+            butt.setBackgroundColor(Color.WHITE);
+            butt.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+            butt.setHeight(findViewById(R.id.add_rat_button).getHeight());
+            butt.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Rat_Sightings_Activity.viewRat(v);
+                }
+            });
+            layout.addView(butt);
+        }
+    }
+
+    public void reload(View v) {//I thought this would be nice to have. We can get rid of it if we need
+        // updates ratListStandIn
+        Rat_Sightings_Activity.setRatList();
+        LinearLayout layout = (LinearLayout) v.getParent();
+        int larger = (ratListStandIn.length>oldRatListStandIn.length?
+                ratListStandIn.length: oldRatListStandIn.length);
+        int smaller =(ratListStandIn.length<oldRatListStandIn.length?
+                ratListStandIn.length: oldRatListStandIn.length);
+        for (int i = 0; i < larger; i++) {//adds new buttons
+            if(i < smaller){ //renaming buttons still on screen
+                Button butt = (Button)findViewById(i);
+                butt.setText("Rat: " + ratListStandIn[i]);//changed later
+            } else if(ratListStandIn.length > oldRatListStandIn.length){//adds extra if adding
+                Button butt = new Button(this);
+
+                butt.setText("Rat: " + ratListStandIn[i]);//changed later
+                butt.setId(i);
+                butt.setBackgroundColor(Color.WHITE);
+                butt.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+                butt.setHeight(findViewById(R.id.add_rat_button).getHeight());
+                butt.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Rat_Sightings_Activity.viewRat(v);
+                    }
+                });
+                layout.addView(butt);
+            } else{//removes extra if removing
+                layout.removeView(findViewById(i));
+            }
+        }
+    }
+
+    public void onBack(View v){
+        startActivity(new Intent(Rat_Sightings_Activity.this, Home_Activity.class));
+    }
+
+    public void addRat(View v){
+        //dummy method to be implemented later
+    }
+
+    public static void viewRat(View v){
+        //dummy method to be implemented later
+        //v.getParent()
+        //^probably must use this method
+    }
+
+    public static void setRatList(){//will be a moved to rat class later
+        String[] temp = ratListStandIn;
+        ratListStandIn = oldRatListStandIn;
+        oldRatListStandIn = temp;
+    }
+
+
+}
