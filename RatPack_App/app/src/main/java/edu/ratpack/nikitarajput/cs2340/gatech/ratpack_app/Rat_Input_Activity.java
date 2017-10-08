dc679533115d9e77a09197885e04a4d1e5e5dfe3
Jacob Kim <jkim3213@gatech.edu>
2017-10-07 23:28:11 -0400

    Small xml changes

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Rat_Input_Activity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/Rat_Input_Activity.java
@@ -2,15 +2,28 @@ package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
-public class Rat_Input_Activity extends AppCompatActivity {
+public class Rat_Input_Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    EditText ratName, address, zipCode, city;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rat__input);
+
+        ratName = (EditText)findViewById(R.id.rat_name_editText);
+        address = (EditText)findViewById(R.id.incident_address_editText);
+        zipCode = (EditText)findViewById(R.id.incident_zip_editText);
+        city = (EditText)findViewById(R.id.incident_city_editText);
+
+
 
         Spinner locationTypeSpinner = (Spinner) findViewById(R.id.spinner_location_type);
         ArrayAdapter<CharSequence> locationTypeAdapter = ArrayAdapter.createFromResource(this,
@@ -19,9 +32,34 @@ public class Rat_Input_Activity extends AppCompatActivity {
         locationTypeSpinner.setAdapter(locationTypeAdapter);
 
         Spinner boroughSpinner = (Spinner) findViewById(R.id.spinner_borough);
-        ArrayAdapter<CharSequence> boroughAdapater = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> boroughAdapter = ArrayAdapter.createFromResource(this,
                 R.array.boroughs, android.R.layout.simple_spinner_item);
-        boroughAdapater.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        boroughSpinner.setAdapter(boroughAdapater);
+        boroughAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        boroughSpinner.setAdapter(boroughAdapter);
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
