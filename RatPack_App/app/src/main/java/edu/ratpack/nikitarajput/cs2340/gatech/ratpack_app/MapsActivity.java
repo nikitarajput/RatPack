17c6158a4d1a0d7797524c24293fd461a71a79ed
Shreya Keshive <shreyakeshive@gatech.edu>
2017-10-21 22:11:06 -0400

    Added marker functionality

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/MapsActivity.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/MapsActivity.java
@@ -2,6 +2,7 @@ package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +11,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app.model.Rat;
+import edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app.model.RatFB;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static Rat[] ratList =new Rat[0];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +32,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng nyc = new LatLng(40.7128, -73.935242);
-        mMap.addMarker(new MarkerOptions().position(nyc).title("Marker in NYC"));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(nyc));
         mMap.setMinZoomPreference(12);
+
+        Rat[] currentRatList= RatFB.getAllRats();
+        for (int i = 0; i < currentRatList.length; i++) {
+            double currLat = currentRatList[i].getLatitude();
+            double currLong = currentRatList[i].getLongitude();
+            LatLng currLatLong = new LatLng(currLat, currLong);
+            mMap.addMarker(new MarkerOptions().position(currLatLong).title("Marker" + i));
+        }
     }
+
 }
