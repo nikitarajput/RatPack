cfa36104eeaf3ee080a3200efda108d7d0ff7390
Soni Aggarwal <sonixaggarwal@gmail.com>
2017-11-10 12:47:27 -0500

    Start of tests before trying Mockito

--- RatPack_App/app/src/test/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/setDataTester.java
+++ RatPack_App/app/src/test/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/setDataTester.java
@@ -5,10 +5,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runners.Parameterized;
 
-import edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app.model.Rat;
-import edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app.model.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app.model.GraphLogic;
 
 /**
  * Created by soniaggarwal on 11/8/17.
@@ -16,96 +16,51 @@ import static org.junit.Assert.assertEquals;
 
 public class setDataTester {
     @Parameterized.Parameter
-    static Rat test;
+    static List<ArrayList<Integer>> test;
+
     @Parameterized.Parameter
-    static String[] input;
+    static int startYear;
+
+    @Parameterized.Parameter
+    static int startMonthInt;
+
+    @Parameterized.Parameter
+    static int endYear;
+
+    @Parameterized.Parameter
+    static int endMonthInt;
 
     @Test
     public void invalid_nums() throws Exception {
-        test = new Rat("No Name(CSV)", -1, -1, "locationType", "address", "city", -1, "borough");
-        input = new String[]{"unimportant", "locationType","Zip", "address", "city", "borough", "Longitude", "latitude"};
-        assertEquals(test.getAddress(), Reader.makeRat(input).getAddress());
-        assertEquals(test.getLongitude(), Reader.makeRat(input).getLongitude(), 0.000000000001);
-        assertEquals(test.getLatitude(), Reader.makeRat(input).getLatitude(), 0.0000000000001);
-        assertEquals(test.getZipCode(), Reader.makeRat(input).getZipCode());
-        assertEquals(test.getName(), Reader.makeRat(input).getName());
-        assertEquals(test.getLocationType(), Reader.makeRat(input).getLocationType());
-        assertEquals(test.getCity(), Reader.makeRat(input).getCity());
-        assertEquals(test.getBorough(), Reader.makeRat(input).getBorough());
+        GraphLogic graphLogic = new GraphLogic(2017, 1, 2017, 1);
+        test = graphLogic.setData(2017, 1, 2017, 1);
     }
     @Test
     public void large_nums() throws Exception {
-        test = new Rat("No Name(CSV)", -1, -1, "locationType", "address", "city", -1, "borough");
-        input = new String[]{"unimportant", "locationType",
-                "54354354354354354354354354354354354354543543543543543543543543543543543543543545435435435435435435435435435435435435435435435435435",
-                "address", "city", "borough",
-                "5435435435435435435435435435435435435454354354354354354354354354354354354354354543543543543543543543543543543543543543543543543543554" +
-                        "35435435435435435435435435435435435454354354354354354354354354354354354354354543543543543543543543543543543543543543543543543" +
-                        "543554354354354354354354354354354354354354543543543543543543543543543543543543543545435435435435435435435435435435435435435435435435435",
-                "-543543543543543543543543543543543543545435435435435435435435435435435435435435454354354354354354354354354354354354354354354354354355435" +
-                        "435435435435435435435435435435435454354354354354354354354354354354354354354543543543543543543543543543543543543543543543543543" +
-                        "554354354354354354354354354354354354354543543543543543543543543543543543543543545435435435435435435435435435435435435435435435435435"};
-        assertEquals(test.getAddress(), Reader.makeRat(input).getAddress());
-        assertEquals(test.getLongitude(), Reader.makeRat(input).getLongitude(), 0.000000000001);
-        assertEquals(test.getLatitude(), Reader.makeRat(input).getLatitude(), 0.0000000000001);
-        assertEquals(test.getZipCode(), Reader.makeRat(input).getZipCode());
-        assertEquals(test.getName(), Reader.makeRat(input).getName());
-        assertEquals(test.getLocationType(), Reader.makeRat(input).getLocationType());
-        assertEquals(test.getCity(), Reader.makeRat(input).getCity());
-        assertEquals(test.getBorough(), Reader.makeRat(input).getBorough());
-
 
     }
     @Test
     public void null_input() throws Exception {
-        test = new Rat("No Name(CSV)", -1, -1, "N/A", "N/A", "N/A", -1, "N/A");
-        input = null;
-        assertEquals(test.getAddress(), Reader.makeRat(input).getAddress());
-        assertEquals(test.getLongitude(), Reader.makeRat(input).getLongitude(), 0.000000000001);
-        assertEquals(test.getLatitude(), Reader.makeRat(input).getLatitude(), 0.0000000000001);
-        assertEquals(test.getZipCode(), Reader.makeRat(input).getZipCode());
-        assertEquals(test.getName(), Reader.makeRat(input).getName());
-        assertEquals(test.getLocationType(), Reader.makeRat(input).getLocationType());
-        assertEquals(test.getCity(), Reader.makeRat(input).getCity());
-        assertEquals(test.getBorough(), Reader.makeRat(input).getBorough());
+
     }
+
     @Test
     public void normal() throws Exception {
-        test = new Rat("No Name(CSV)", 68.765454545, 33.6565656, "hospital", "2886 Northbrook Dr", "Atlanta", 30344, "Bronx");
-        input = new String[]{"unimportant", "hospital","30344", "2886 Northbrook Dr", "Atlanta", "Bronx", "33.6565656", "68.765454545"};
-        assertEquals(test.getAddress(), Reader.makeRat(input).getAddress());
-        assertEquals(test.getLongitude(), Reader.makeRat(input).getLongitude(), 0.000000000001);
-        assertEquals(test.getLatitude(), Reader.makeRat(input).getLatitude(), 0.0000000000001);
-        assertEquals(test.getZipCode(), Reader.makeRat(input).getZipCode());
-        assertEquals(test.getName(), Reader.makeRat(input).getName());
-        assertEquals(test.getLocationType(), Reader.makeRat(input).getLocationType());
-        assertEquals(test.getCity(), Reader.makeRat(input).getCity());
-        assertEquals(test.getBorough(), Reader.makeRat(input).getBorough());
+
     }
 
     @Test
     public void incomplete_inputs() throws Exception {
-        test = new Rat("No Name(CSV)", -1, -1, "hospital", "2886 Northbrook Dr", "Atlanta", 30344, "N/A");
-        input = new String[]{"unimportant", "hospital","30344", "2886 Northbrook Dr", "Atlanta"};
-        assertEquals(test.getAddress(), Reader.makeRat(input).getAddress());
-        assertEquals(test.getLongitude(), Reader.makeRat(input).getLongitude(), 0.000000000001);
-        assertEquals(test.getLatitude(), Reader.makeRat(input).getLatitude(), 0.0000000000001);
-        assertEquals(test.getZipCode(), Reader.makeRat(input).getZipCode());
-        assertEquals(test.getName(), Reader.makeRat(input).getName());
-        assertEquals(test.getLocationType(), Reader.makeRat(input).getLocationType());
-        assertEquals(test.getCity(), Reader.makeRat(input).getCity());
-        assertEquals(test.getBorough(), Reader.makeRat(input).getBorough());
+
     }
 
     @BeforeClass
     public static void build(){
-        test = new Rat();
-        input = new String[8];
+        test = new ArrayList<>();
     }
 
     @AfterClass
     public static void tearDown(){
         test = null;
-        input = null;
     }
 }
