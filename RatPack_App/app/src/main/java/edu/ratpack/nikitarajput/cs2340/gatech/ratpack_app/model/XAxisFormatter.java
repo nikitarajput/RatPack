c074d5df267f3f05ae73b2d7a87b18865d2c77a9
jkim3213 <jkim3213@gatech.edu>
2017-11-13 03:23:04 -0500

    Cleaned up XAxisFormatter

--- RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/model/XAxisFormatter.java
+++ RatPack_App/app/src/main/java/edu/ratpack/nikitarajput/cs2340/gatech/ratpack_app/model/XAxisFormatter.java
@@ -3,21 +3,33 @@ package edu.ratpack.nikitarajput.cs2340.gatech.ratpack_app.model;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
-public class XAxisFormatter implements IAxisValueFormatter {
-    private String[] months;
+import java.util.Arrays;
 
-    public XAxisFormatter(String[] values) {
-        this.months = values;
+/**
+ * Used by Graph Tool to create XAxis
+ */
+public class XAxisFormatter implements IAxisValueFormatter {
+    private final String[] months;
+
+    /**
+     * sets the months to values
+     * @param values the String values which are given by GraphLogic
+     */
+    XAxisFormatter(String[] values) {
+        months = Arrays.copyOf(values, values.length);
     }
 
+    /**
+     * Returns the value for each data point
+     * @param value the xAxis number
+     * @param axis the Axis to format
+     * @return The string to replace the value
+     */
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
         // "value" represents the position of the label on the axis (x or y)
         return months[(int) value];
     }
 
-    /** this is only needed if numbers are returned, else return 0 */
-
-    public int getDecimalDigits() { return 0; }
 }
 
